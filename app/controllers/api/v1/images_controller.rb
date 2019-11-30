309ebfd519d6da219c9c5bf356ca5484868f8cf5
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-11-30 16:16:06 -0500

    remove unnecessary code

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,13 +1,6 @@
 module Api::V1
   class ImagesController < ApplicationApiController
     def index
-      puts "api at work"
-      if params[:page]
-        response = {status: "sending pages.."}
-      else
-        response = {status: "sending all images.."}
-      end
-
       imagesArray = File.readlines("public/images.csv").map { |line| {url: line.chop} }
       if params[:page]
         if params[:page] == '1'
