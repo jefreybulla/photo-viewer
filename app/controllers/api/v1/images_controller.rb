3ccbe4bf36681fa21e5ec879c265c41d2b66a999
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-11-30 16:12:41 -0500

    create api to parse and serve images in pages

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -4,12 +4,24 @@ module Api::V1
       puts "api at work"
       if params[:page]
         response = {status: "sending pages.."}
-        #todo: parse csv file.
-        #puts File.read("test.txt")
       else
         response = {status: "sending all images.."}
       end
-      render json: response
+
+      imagesArray = File.readlines("public/images.csv").map { |line| {url: line.chop} }
+      if params[:page]
+        if params[:page] == '1'
+          resultsArray = imagesArray[0..24]
+        elsif params[:page] == '2'
+          resultsArray =  imagesArray[25..49]
+        else
+          resultsArray = "no images"
+        end
+      else
+        resultsArray = imagesArray
+      end
+
+      render json: {results: resultsArray.length, images: resultsArray}
     end
   end
 end
