ac524c8afe473253cbae0d0e7bedc60b93f94a7f
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-11-30 11:03:57 -0500

    add comment

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -4,6 +4,7 @@ module Api::V1
       puts "api at work"
       if params[:page]
         response = {status: "sending pages.."}
+        #todo: parse csv file. 
       else
         response = {status: "sending all images.."}
       end
