2dc21bfbb6c1e574dbac5d463c7271097b784a69
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-11-30 11:25:14 -0500

    remove sql3 gem

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -4,7 +4,8 @@ module Api::V1
       puts "api at work"
       if params[:page]
         response = {status: "sending pages.."}
-        #todo: parse csv file. 
+        #todo: parse csv file.
+        #puts File.read("test.txt")
       else
         response = {status: "sending all images.."}
       end
