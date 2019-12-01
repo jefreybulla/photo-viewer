8902f36b893bf60450bae74d1392d5ba18c4e53b
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-12-01 09:09:14 -0500

    add api call to react component

--- app/javascript/components/Gallery.js
+++ app/javascript/components/Gallery.js
@@ -1,51 +1,65 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const api = "/api/v1/images?page=1"
+
 class Gallery extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      apiResponse: [],
+      status: "loading",
+    };
+  }
+
+  componentDidMount(){
+    fetch(api,  {method:"GET"})
+      .then(response => response.json())
+      .then(data => this.setState({ apiResponse: data }))
+      .then(result => this.onSetResult(result));   //called after api call is completed
+  }
+
+  onSetResult(){
+    console.log("api call completed");
+    console.log(this.state.apiResponse.images[0].url);
+    this.setState({ status: "ready" });
+  }
+
+  renderImage(){
+    let numberOfImages = 20
+    let items = [];
+    for(let i=0;i<=this.state.apiResponse.results-1;i++){
+      items.push(
+          <div className="col-3" key={i}>
+            <img src={this.state.apiResponse.images[i].url} className="img-thumbnail" />
+          </div>
+      );
+    }
+    return items;
+  }
+
+
   render () {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col ml-1">
-            <h1 >Hello</h1>
+    if (this.state.status == "loading"){
+      return (
+        <div>Loading images...</div>
+      );
+    }
+    else{
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col ml-1">
+              <h1 >Photo Gallery</h1>
+            </div>
+          </div>
+          <div className="row mt-2">
+            {this.renderImage()}
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <img src="https://picsum.photos/id/256/100/100" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/323/400/200" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/1021/250/250" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/180/300/200" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/319/400/200" className="img-thumbnail" />
-          </div>
-        </div>
-        <div className="row mt-2">
-          <div className="col">
-            <img src="https://picsum.photos/id/256/100/100" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/256/100/100" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/256/100/100" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/256/100/100" className="img-thumbnail" />
-          </div>
-          <div className="col">
-            <img src="https://picsum.photos/id/256/100/100" className="img-thumbnail" />
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
+
   }
 }
 
