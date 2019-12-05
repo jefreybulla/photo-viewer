345ba28e3b190c5b77a54f5fec9e8b7b594a8bc5
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-12-05 11:31:24 -0500

    create api response with size filters. WIP: front-end filters

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,23 +1,32 @@
 module Api::V1
   class ImagesController < ApplicationApiController
     def index
-      #Create an array of objects to be return with the format {url: image_url}
+      #Create an array of objects to be returned with the format {url: image_url}
       imagesArray = File.readlines("public/images.csv").map { |line| {url: line.chop} }
 
-      #Handle pages if request includes ?page=page_number.
-      if params[:page]
-        if params[:page] == '1'
-          resultsArray = imagesArray[0..19]
-        elsif params[:page] == '2'
-          resultsArray =  imagesArray[20..39]
-        elsif params[:page] == '3'
-          resultsArray =  imagesArray[40..49]
-        else
-          resultsArray = "no images"
+      resultsArray = []
+
+      #filter by size if request includes ?width=value&height=value
+      if params[:width] && params[:height]
+        imagesArray.each do |image|
+          m = image[:url].match(/id(.)(?<id>\d+)(.)(?<width>\d+)(.)(?<height>\d+)/)
+          if m[:width] == params[:width] && m[:height] == params[:height]
+            resultsArray.push(image)
+          end
         end
       else
-        #Return all images if request does not include ?page
         resultsArray = imagesArray
+      end
+
+      #Handle pages if request includes ?page=value
+      if params[:page]
+        itemsPerPage = 20
+        firstItem = (params[:page].to_i - 1)*itemsPerPage
+        if resultsArray[firstItem]
+          resultsArray = resultsArray[firstItem..firstItem+(itemsPerPage-1)]
+        else
+          resultsArray = []
+        end
       end
 
       render json: {results: resultsArray.length, images: resultsArray}
--- app/javascript/components/Gallery.js
+++ app/javascript/components/Gallery.js
@@ -10,6 +10,7 @@ class Gallery extends React.Component {
     this.state = {
       apiResponse: [],
       status: "loading",
+      imageSize:[0.0],
     };
   }
 
@@ -30,9 +31,29 @@ class Gallery extends React.Component {
       .then(result => this.onSetResult(result));
   }
 
+  sizeFilter(w,h){
+    console.log(`this is sizeFilter w${w} h${h}`);
+  }
+
   //Method called after api result is received
   onSetResult(){
     this.setState({ status: "ready" });
+  }
+
+  renderFilters(){
+    return (
+      <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Image size
+        </button>
+        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <span className="dropdown-item react-action" onClick={() => this.sizeFilter(0,0)}>Any size</span>
+          <span className="dropdown-item react-action" onClick={() => this.sizeFilter(100,100)}>100x100</span>
+          <span className="dropdown-item react-action" onClick={() => this.sizeFilter(250,250)}>250x250</span>
+          <span className="dropdown-item react-action" onClick={() => this.sizeFilter(300,200)}>300x200</span>
+        </div>
+      </div>
+    );
   }
 
   //Method to render image thumbnails
@@ -52,7 +73,7 @@ class Gallery extends React.Component {
   renderPageButton(page){
     return(
       <div className="col-1" onClick={() => this.handlePage(page)}>
-        {page}
+        <span className="react-action">{page}</span>
       </div>
     )
   }
@@ -69,10 +90,13 @@ class Gallery extends React.Component {
         <div className="container">
           <div className="row">
             <div className="col ml-1">
-              <h1 >Photo Gallery</h1>
+              <h1>Photo Gallery</h1>
             </div>
           </div>
-          <div className="row mt-2">
+          <div className="row ml-1 mt-1">
+            {this.renderFilters()}
+          </div>
+          <div className="row mt-3">
             {this.renderImage()}
           </div>
           <div className="row mt-5 justify-content-center">
